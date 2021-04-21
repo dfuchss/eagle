21394e4eed64cc424282488da11782dba3aee76f
Dominik Fuchß <dominik.fuchss@kit.edu>
2021-04-21 19:18:14 +0200

    Reworked NoExploration

--- xplore/src/main/java/edu/kit/ipd/eagle/impl/xplore/selection/NoExploration.java
+++ xplore/src/main/java/edu/kit/ipd/eagle/impl/xplore/selection/NoExploration.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import edu.kit.ipd.eagle.port.hypothesis.HypothesesSelection;
 import edu.kit.ipd.eagle.port.hypothesis.IHypothesesSelection;
 import edu.kit.ipd.eagle.port.hypothesis.IHypothesesSet;
+import edu.kit.ipd.eagle.port.hypothesis.IHypothesis;
 import edu.kit.ipd.eagle.port.xplore.selection.ISelectionProvider;
 
 /**
@@ -16,13 +17,30 @@ import edu.kit.ipd.eagle.port.xplore.selection.ISelectionProvider;
  */
 public class NoExploration implements ISelectionProvider {
 
-	@Override
-	public List<List<IHypothesesSelection>> findSelection(List<IHypothesesSet> hypotheses) {
-		return List.of( //
-				hypotheses.stream() //
-						.filter(hs -> !hs.getHypotheses().isEmpty()) //
-						.map(hs -> new HypothesesSelection(hs, List.of(hs.getSortedHypotheses().get(0)))) //
-						.collect(Collectors.toList()));
-	}
+    @Override
+    public List<List<IHypothesesSelection>> findSelection(List<IHypothesesSet> hypotheses) {
+        return List.of(//
+                hypotheses.stream() //
+                        .filter(hs -> !hs.getHypotheses().isEmpty()) //
+                        .map(this::createSelection) //
+                        .collect(Collectors.toList()));
+    }
 
+    private IHypothesesSelection createSelection(IHypothesesSet hypotheses) {
+        List<IHypothesis> hyps = hypotheses.getHypotheses();
+        double confidence = hyps.get(0).getConfidence();
+        boolean onlyOneConfidence = true;
+        for (IHypothesis h : hyps) {
+            if (Math.abs(h.getConfidence() - confidence) > 1E-8) {
+                onlyOneConfidence = false;
+                break;
+            }
+        }
+
+        if (onlyOneConfidence) {
+            return new HypothesesSelection(hypotheses, List.of(hyps.get(0)));
+        }
+
+        return new HypothesesSelection(hypotheses, List.of(hypotheses.getSortedHypotheses().get(0)));
+    }
 }
